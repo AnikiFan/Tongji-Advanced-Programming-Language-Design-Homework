1b4ebe8eaf4137df356089c7cf253d7dae91ad1a
AnikiFan <15800788661@163.com>
2023-03-10 12:10:51 +0800

    test

---
+++ c/week 3/cpptest1/test.cpp
@@ -0,0 +1,13 @@
+/* warning, this file is reserved for testing file only*/
+/* warning, this file is reserved for testing file only*/
+/* warning, this file is reserved for testing file only*/
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a = 10, b = 5;
+	cout << "a=" << a << ", " << "b=" << b << endl;
+	cout << "Hello, Welcome!\n" ;
+	cout << "Hello, Welcome\x21\n";
+	return 0;
+}
---
+++ c/week 3/ctest1/ctest1/test.c
@@ -0,0 +1,15 @@
+/* warning, this file is reserved for testing file only*/
+/* warning, this file is reserved for testing file only*/
+/* warning, this file is reserved for testing file only*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+    int  ret;
+
+    char s[80], t[80];
+
+    ret = scanf("%s,%s", s, t);
+    printf(" ret=%d\n",  ret);
+    return 0;
+}
