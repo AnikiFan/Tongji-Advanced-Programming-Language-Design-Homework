ed8545682a127942c54a5e53404719038e69c306
AnikiFan <15800788661@163.com>
2023-03-23 18:30:37 +0800

    updated

--- cpp/week5/cpphomeworkweek5/cpphomeworkweek5/3-b12-1.cpp
+++ cpp/week5/cpphomeworkweek5/cpphomeworkweek5/3-b12-1.cpp
@@ -4,17 +4,19 @@ using namespace std;
 
 int main()
 {
-	int x;
+	int x,state;
 
 	while (1) {
 		cout << "请输入x的值[0-100] : ";
 		cin >> x;   //读入x的方式必须是 cin>>int型变量，不允许其他方式
-		if (cin.fail()) {
-			cin.clear();
-			cin.sync();
-		}
-		else if (x >= 0 && x <= 100)
+		state=cin.rdstate();
+		if (x >= 0 && x <= 100&& !state)
 			break;
+		else if (state) {
+			cin.clear(0);	
+			cin.sync();
+			cin.ignore(10000000,'\n');//删了这个以后会死循环
+		}
 	}
 
 	cout << "cin.good()=" << cin.good() << " x=" << x << endl; //此句不准动，并且要求输出时good为1
--- cpp/week5/cpphomeworkweek5/打印月历/3-b13-1.cpp
+++ cpp/week5/cpphomeworkweek5/打印月历/3-b13-1.cpp
@@ -3,7 +3,12 @@
 using namespace std;
 int main()
 {
-	cout << "" << endl;
-	cin >> ;
+	int year, month, week;
+	cout << "请输入年份(2000-2030)和月份(1-12) : " << endl;
+	cin >> year>>month;
+	cout << "请输入"<<year<<"年"<<month<<"月1日的星期(0 - 6表示星期日 - 星期六) : " << endl;
+	cin >> week;
+	cout << year<<"年"<<month<<"月的月历为: " << endl;
+	cout << "星期日  星期一  星期二  星期三  星期四  星期五  星期六" << endl;
 	return 0;
 }
