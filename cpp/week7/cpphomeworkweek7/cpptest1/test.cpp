aa78504ce043a7090ecd4555f48976a0b79ed9fd
AnikiFan <15800788661@163.com>
2023-04-08 00:39:27 +0800

    stage1finished

--- cpp/week7/cpphomeworkweek7/cpptest1/test.cpp
+++ cpp/week7/cpphomeworkweek7/cpptest1/test.cpp
@@ -1,97 +1,24 @@
 /* 2254298 信11 范潇 */
 #include <iostream>
 #include <iomanip>
-#include <cstdio>
+#include <Windows.h>
 using namespace std;
-
-/* -----------------------------------------------------------------------------------
-		允许   ：1、按需增加一个或多个函数（包括递归函数），但是所有增加的函数中不允许任何形式的循环
-				 2、定义符号常量
-				 3、定义const型变量
-
-		不允许 ：1、定义全局变量
-				 2、除print_tower之外的其他函数中不允许定义静态局部变量
-   ----------------------------------------------------------------------------------- */
-
-
-char splitter(int n)
-{
-	if (n == 1)
-		return('=');
-	if (n == 2) {
-		cout << "=";
-		return('=');
-	}
-	cout << "=";
-	splitter(n - 1);
-	return('=');
-}
-
-void ALPHAstring(int order, char endalpha)//起始字母为A,正序则order为1
-{
-	if (order) {
-		if (endalpha == 'A') {
-			cout << 'A';
-			return;
-		}
-		ALPHAstring(order, endalpha - 1);
-		cout << endalpha;
-		return;
-	}
-	else {
-		if (endalpha == 'A') {
-			cout << 'A';
-			return;
-		}
-		cout << endalpha;
-		ALPHAstring(order, endalpha - 1);
-		return;
-	}
-}
-void singlelevel(char maxchar, char levelchar)//levelchar即该层中心字母
-{
-	cout << setw(maxchar - levelchar);
-	if (levelchar != 'A') {
-		ALPHAstring(1, levelchar);
-		ALPHAstring(0, levelchar - 1);
-		cout << endl;
-	}
-	else
-		cout << 'A' << endl;
-	return;
-}
+	long long sum=0;
 /***************************************************************************
   函数名称：
-  功    能：打印字母塔
+  功    能：返回fibonacci数列的第n项的值
   输入参数：
   返 回 值：
-  说    明：形参按需设置
-			提示：有一个参数order，指定正序/倒序
+  说    明：不允许任何形式的循环出现
 ***************************************************************************/
-void print_tower(int order, char maxchar)//order=1时为正序
+int fibonacci(int n)
 {
-	/* 允许按需定义最多一个静态局部变量（也可以不定义） */
-	static char MAXCHAR = maxchar;
-	if (order) {
-		if (maxchar == 'A') {
-			singlelevel(MAXCHAR, maxchar);
-			return;
-		}
-		print_tower(order, maxchar - 1);
-		singlelevel(MAXCHAR, maxchar);
-		return;
+	if (n == 1 || n == 2) {
+		sum++;
+		return 1;
 	}
-	else {
-		if (maxchar == 'A') {
-			singlelevel(MAXCHAR, maxchar);
-			return;
-		}
-		singlelevel(MAXCHAR, maxchar);
-		print_tower(order, maxchar - 1);
-		return;
-	}
-	/* 按需实现，函数中不允许任何形式的循环，函数允许调用其它函数 */
-
+	sum++;
+	return(fibonacci(n - 2) + fibonacci(n - 1));
 }
 
 /***************************************************************************
@@ -99,41 +26,15 @@ void print_tower(int order, char maxchar)//order=1时为正序
   功    能：
   输入参数：
   返 回 值：
-  说    明：main函数中的...允许修改，其余位置不准修改
+  说    明：main函数不允许修改
 ***************************************************************************/
 int main()
 {
-	char end_ch;
-
-	/* 键盘输入结束字符(仅大写有效，为避免循环出现，不处理输入错误) */
-	cout << "请输入结束字符(A~Z)" << endl;
-	end_ch = getchar();			//读缓冲区第一个字符
-	if (end_ch < 'A' || end_ch > 'Z') {
-		cout << "结束字符不是大写字母" << endl;
-		return -1;
+	int i;
+	for (i=3; i <= 46; i++) {
+		sum = 0;
+		fibonacci(i);
+		cout <<"n="<<setw(2)<<i<<"时,递归次数为" << sum << endl;
 	}
-
-	/* 正三角字母塔(两边为A) */
-	cout << splitter(2 * (end_ch - 'A') + 1) << endl; /* 按字母塔最大宽度输出=(不允许用循环) */
-	cout << "正三角字母塔(A->" << end_ch << ")" << endl;
-	cout << splitter(2 * (end_ch - 'A') + 1) << endl; /* 按字母塔最大宽度输出=(不允许用循环) */
-	print_tower(1, end_ch); //正序打印 A~结束字符 
-	cout << endl;
-
-	/* 倒三角字母塔(两边为A) */
-	cout << splitter(2 * (end_ch - 'A') + 1) << endl; /* 按字母塔最大宽度输出=(不允许用循环) */
-	cout << "倒三角字母塔(A->" << end_ch << ")" << endl;
-	cout << splitter(2 * (end_ch - 'A') + 1) << endl; /* 按字母塔最大宽度输出=(不允许用循环) */
-	print_tower(0, end_ch); //逆序打印 A~结束字符 
-	cout << endl;
-
-	/* 合起来就是漂亮的菱形（外侧为A） */
-	cout << splitter(2 * (end_ch - 'A') + 1) << endl;/* 按字母塔最大宽度输出= */
-	cout << "菱形(A->" << end_ch << ")" << endl;
-	cout << splitter(2 * (end_ch - 'A') + 1) << endl;/* 按字母塔最大宽度输出= */
-	print_tower(1, end_ch);   //打印 A~结束字符的正三角 
-	print_tower(0, end_ch);   //打印 A~结束字符-1的倒三角 
-	cout << endl;
-
 	return 0;
 }
--- cpp/week7/cpphomeworkweek7/汉诺塔-基本移动/4-b12.cpp
+++ cpp/week7/cpphomeworkweek7/汉诺塔-基本移动/4-b12.cpp
@@ -1,8 +1,8 @@
 /* 2254298 信11 范潇 */
 #include <iostream>
 #include <iomanip>
+#define arrow "-->"
 using namespace std;
-
 /* ----具体要求----
    1、不允许添加其它头文件
    2、不允许定义全局变量、静态局部变量
@@ -21,8 +21,17 @@ using namespace std;
 	 说    明：1、函数名、形参、返回类型均不准动
 			   2、本函数不允许出现任何形式的循环
    ***************************************************************************/
+//对于n层汉诺塔,先将起始柱上的n-1层移至中间柱,然后将留在起始柱的底层移至目标柱,再将中间柱上的n-1层移至目标柱上.
 void hanoi(int n, char src, char tmp, char dst)
 {
+	if (n == 1) {
+		cout << " 1# " << src << arrow << dst << endl;
+		return;
+	}
+	hanoi(n - 1, src, dst, tmp);
+	cout << setw(2) << n << "# " << src << arrow << dst << endl;
+	hanoi(n - 1, tmp, src, dst);
+	return;
 }
 
 /***************************************************************************
@@ -34,6 +43,66 @@ void hanoi(int n, char src, char tmp, char dst)
 			2、处理输入错误时，允许使用循环
 			3、为了统一检查，不再允许添加其它函数（输入起始/目标柱的代码不要求统一函数处理，均直接放在main中）
 ***************************************************************************/
+
+
 int main()
 {
+	int n;
+	char src, tmp, dst;
+	cout << "请输入汉诺塔的层数(1-16)" << endl;
+	while (1) {
+		cin >> n;
+		if (n >= 0 && n <= 16 &&  !cin.rdstate())
+			break;
+			cin.clear();
+			cin.ignore(10000000, '\n');//删了这个以后会死循环
+			cout << "请输入汉诺塔的层数(1-16)" << endl;
+	}
+	cout << "请输入起始柱(A-C)" << endl;
+	while (1) {
+		cin >> src;
+		if ((src=='A'|| src == 'B' || src == 'C' || src == 'a' || src == 'b' || src == 'c'  ) && !cin.rdstate())
+			switch (src) {
+				case 'a':
+					src = 'A';
+					break;
+				case 'b':
+					src = 'B';
+					break;
+				case 'c':
+					src = 'C';
+					break;
+			}
+			break;
+		cin.clear();
+		cin.ignore(10000000, '\n');//删了这个以后会死循环
+		cout << "请输入起始柱(A-C)" << endl;
+	}
+	cout << "请输入目标柱(A-C)" << endl;
+	while (1) {
+		cin >> dst;
+		if ((dst == 'A' || dst == 'B' || dst == 'C' || dst == 'a' || dst == 'b' || dst == 'c') && !cin.rdstate()
+			&&dst!=src&&dst!=(src-32)&& dst != (src+32))
+			break;
+		cin.clear();
+		cin.ignore(10000000, '\n');//删了这个以后会死循环
+		if(dst == src || dst == (src - 32) || dst == (src + 32))
+			cout << "目标柱(" << src << ")不能与起始柱(" << src << ")相同"<<endl;
+		cout << "请输入目标柱(A-C)" << endl;
+	}
+	switch (dst) {
+		case 'a':
+			dst = 'A';
+			break;
+		case 'b':
+			dst = 'B';
+			break;
+		case 'c':
+			dst = 'C';
+			break;
+	}
+	cout << "移动步骤为:" << endl;
+	tmp = int(src != 'A') * int(dst != 'A') * 'A' + int(src != 'B') * int(dst != 'B') * 'B' + int(src != 'C') *int( dst != 'C') * 'C';
+	hanoi(n, src, tmp, dst);
+	return 0;
 }
