87bad0fe83dbd5b06ae85417ac83beb565c9a61b
AnikiFan <15800788661@163.com>
2023-05-04 10:42:53 +0800

    stage1
    菜单函数以及main函数大循环完成

---
+++ PROJECT1/汉诺塔大作业/cpptest1/input.cpp
@@ -0,0 +1,18 @@
+﻿while (1) {
+	bool input = true;
+	for (i = 0; i < 113000; i++)
+		binary[i] = '0';
+	cin >> binary;
+	for (i = 1; i < 113; i++)
+		if (binary[i] != '0' && binary[i] != '1' && binary[i] != 0)
+			input = false;
+	if (input && binary[0] == '.' && !cin.rdstate())
+		break;
+	cin.clear();
+	cin.ignore(10000000, '\n');//É¾ÁËÕâ¸öÒÔºó»áËÀÑ­»·
+	if (binary[0] != '.')
+		cout << "²»ÊÇÒÔ.¿ªÍ·£¬ÇëÖØÊä" << endl;
+	else
+		cout << "ÊäÈëµÄ²»ÊÇ0/1£¬ÇëÖØÊä" << endl;
+
+}
---
+++ PROJECT1/汉诺塔大作业/cpptest2/test.cpp
@@ -0,0 +1,10 @@
+using namespace std;
+#include<iostream>
+#include<conio.h>
+int main()
+{
+	int input;
+	input = _getch();
+	cout << input;
+	return 0;
+}
