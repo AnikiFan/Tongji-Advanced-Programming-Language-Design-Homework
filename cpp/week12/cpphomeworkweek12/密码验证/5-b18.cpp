5e58ea9d8f618d4fb74ad2ac2df3ae9e13b4ff3c
AnikiFan <15800788661@163.com>
2023-05-16 00:03:02 +0800

    refined
    考虑了不在范围内的其他字符已经输入错误

--- cpp/week12/cpphomeworkweek12/密码验证/5-b18.cpp
+++ cpp/week12/cpphomeworkweek12/密码验证/5-b18.cpp
@@ -15,50 +15,57 @@ int main()
 {
 	char str[eraseNum],input[getInputNum];
 	int i,j,k,l,num[5],comp[5];
-	bool validOther = false,valideCode=true;
+	bool validOther = false,validCode=true;
 	cin.getline(str, eraseNum, 10);
-	for (i = 0; i < 5; i++)
+	for (i = 0; i < 5; i++) 
 		cin >> num[i];
-	cin.getline(input, getInputNum, 10);;//消除缓存区中的尾零
-	for (i = 0; i < codeNum; i++) {
-		k = 0;
-		for (j = 0; j < 5; j++)
-			comp[j] = num[j];
+	if (!cin.rdstate()) {
+		cin.getline(input, getInputNum, 10);;//消除缓存区中的尾零
+		for (i = 0; i < codeNum; i++) {
+			k = 0;
+			for (j = 0; j < 5; j++)
+				comp[j] = num[j];
 
-		cin.getline(input, getInputNum, 10);
-		while (input[k] != 0) {
-			if (k >= length) {
-				valideCode = false;
-				break;
-			}
-			else if (input[k] >= 'A' && input[k] <= 'Z')
-				uppercaseNum --;
-			else if (input[k] >= 'a' && input[k] <= 'z')
-				lowercaseNum--;
-			else if (input[k] >= '0' && input[k] <= '9')
-				digitNum--;
-			else {
-				for (l = 0; l < validOtherNum; l++)
-					if (input[k] == other[l]) {
-						otherNum--;
-						validOther = true;
-						break;
-					}
-				if (!validOther) 
+			cin.getline(input, getInputNum, 10);
+			while (input[k] != 0) {
+				if (k >= length) {
+					validCode = false;
 					break;
+				}
+				else if (input[k] >= 'A' && input[k] <= 'Z')
+					uppercaseNum--;
+				else if (input[k] >= 'a' && input[k] <= 'z')
+					lowercaseNum--;
+				else if (input[k] >= '0' && input[k] <= '9')
+					digitNum--;
+				else {
+					validOther = false;
+					for (l = 0; l < validOtherNum; l++)
+						if (input[k] == other[l]) {
+							otherNum--;
+							validOther = true;
+							//
+							//cout << input[k] << other[l] << endl;
+							//
+							break;
+						}
+					if (!validOther)
+						validCode = false;
+				}
+				k++;
 			}
-			k++;
+			if (!(uppercaseNum <= 0 && lowercaseNum <= 0 && digitNum <= 0 && otherNum <= 0))
+				//
+				//cout << "*"<<i << endl;
+				//
+				validCode = false;
+			if (!validCode)
+				break;
 		}
-		if (!(uppercaseNum <= 0 && lowercaseNum <= 0 && digitNum <= 0 && otherNum <= 0)) {
-			//
-			cout << "*"<<i << endl;
-			//
-			valideCode = false;
-		}
-		if (!valideCode)
-			break;
 	}
-	if (valideCode)
+	else
+		validCode = false;
+	if (validCode)
 		cout << "正确" << endl;
 	else
 		cout << "错误" << endl;
