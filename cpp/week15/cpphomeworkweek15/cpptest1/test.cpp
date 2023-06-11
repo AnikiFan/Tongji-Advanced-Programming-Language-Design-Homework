c69d701da2d3dd81700a90b247ef877474ead01a
AnikiFan <15800788661@163.com>
2023-06-11 18:07:52 +0800

    高程复习总结全部结束

--- cpp/week15/cpphomeworkweek15/cpptest1/test.cpp
+++ cpp/week15/cpphomeworkweek15/cpptest1/test.cpp
@@ -5,6 +5,25 @@
 using namespace std;
 int main()
 {
-
+	class Time {
+	private:
+		int hour;
+		int minute;
+		int sec;
+	public:
+		Time(int h = 0, int m = 0, int s = 0)
+		{
+			hour = h;
+			minute = m;
+			sec = s;
+		}
+		void display()
+		{
+			cout << hour << ':' << minute << ';' << sec << endl;
+		}
+	};
+	Time t[10] = { (1,2,3),(4,5),6,7,8,9,10 };
+	for (int i = 0; i < 10; i++)
+		t[i].display();
 	return 0;
 }
